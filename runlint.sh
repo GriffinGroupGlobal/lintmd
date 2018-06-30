ef2a590284b65d1cc672132e183124c554143d2b
MattatG3 <matt.jenks@griffingroupglobal.com>
2018-06-29 21:23:41 -0400

    GP-324 ignore node_modules

--- runlint.sh
+++ runlint.sh
@@ -7,9 +7,8 @@ wd=`pwd`
 cd /home/node
 
 if [ -d $wd/src ]; then
-   npm run lint -- -V
-   npm run lint -- $wd/*.md
-   npm run lint -- $wd/**/*.md
+   npm run lint -- -i $wd/node_modules $wd/*.md
+   npm run lint -- -i $wd/node_modules $wd/**/*.md
 fi
 
 
