d11db2202e31382a9dabdf82d9de64f18de9a55e
MattatG3 <matt.jenks@griffingroupglobal.com>
2018-07-02 13:40:26 -0400

    GP-324 remove dependency on src directory

--- runlint.sh
+++ runlint.sh
@@ -6,9 +6,7 @@ wd=`pwd`
 
 cd /home/node
 
-if [ -d $wd/src ]; then
-   npm run lint -- -i $wd/node_modules $wd/*.md
-   npm run lint -- -i $wd/node_modules $wd/**/*.md
-fi
+npm run lint -- -i $wd/node_modules $wd/*.md
+npm run lint -- -i $wd/node_modules $wd/**/*.md
 
 
